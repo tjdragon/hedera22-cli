75813d7b189c27c8d3be7858be5e1ccfd6f1ef8e
tjdragon <thierry@janaudy.com>
2022-04-21 14:48:09 +0200

    v1

--- src/main/java/org/tj/hedera22/PrivateKeys.java
+++ src/main/java/org/tj/hedera22/PrivateKeys.java
@@ -2,11 +2,8 @@ package org.tj.hedera22;
 
 import com.hedera.hashgraph.sdk.PrivateKey;
 
-/**
- * Head to https://portal.hedera.com/register to create your accounts
- */
 public class PrivateKeys {
-    public static final PrivateKey OPERATOR_KEY = PrivateKey.fromString("xx");
-    public static final PrivateKey PARTICIPANT_1_KEY = PrivateKey.fromString("xx");
-    public static final PrivateKey PARTICIPANT_2_KEY = PrivateKey.fromString("xx");
+    public static final PrivateKey OPERATOR_KEY = PrivateKey.fromString("302e020100300506032b657004220420da453cb2ac1ea45b8461cb48cc442a43b8c325e64b68fc7bafb33a92257d096f");
+    public static final PrivateKey PARTICIPANT_1_KEY = PrivateKey.fromString("302e020100300506032b657004220420249ee3e72659a80b4b2aad38ffe03408f6dfe37b7026223d320fd89190c095c3");
+    public static final PrivateKey PARTICIPANT_2_KEY = PrivateKey.fromString("302e020100300506032b657004220420cef0b7f3d3f7c3ccf785af52970fc1057e11fa605343952621f1b761da49b922");
 }
